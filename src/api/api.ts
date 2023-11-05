1ed5d4a0cf439df763a8138b37ff61ce151ac9d9
ross-brown <rbrown_98@yahoo.com>
2023-11-04 23:54:32 -0500

    change env name to VITE instead of REACT_APP

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:3001";
 import {
   LoginFormInterface, MessageInterface,
   SignupFormInterface, ProfileFormInterface,
