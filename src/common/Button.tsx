6900f5f8b665d7d73b0889ed5f82e75ffbb0b315
ross-brown <rbrown_98@yahoo.com>
2024-01-22 17:35:00 -0600

    fix dynamic tailwind color bug with variant object

--- src/common/Button.tsx
+++ src/common/Button.tsx
@@ -7,13 +7,16 @@ interface ButtonProps {
 }
 
 function Button({ children, color, onClick }: ButtonProps) {
+  const colorVariants = {
+    green: "bg-green-600 hover:bg-green-500 focus:ring-green-400 active:bg-green-700",
+    rose: "bg-rose-600 hover:bg-rose-500 focus:ring-rose-400 active:bg-rose-700",
+  };
+
   return (
     <button
       onClick={onClick}
-      className={`mt-4 block px-5 py-3 rounded-lg
-      bg-${color}-600 hover:bg-${color}-500 focus:outline-none
-      focus:ring focus:ring-offset-2 focus:ring-${color}-400
-      focus:ring-opacity-50 active:bg-${color}-700
+      className={`${colorVariants[color]} mt-4 block px-5 py-3 rounded-lg focus:outline-none
+      focus:ring focus:ring-offset-2 focus:ring-opacity-50
       text-white shadow-lg uppercase tracking-wider
       font-semibold text-sm sm:text-base`}
     >
