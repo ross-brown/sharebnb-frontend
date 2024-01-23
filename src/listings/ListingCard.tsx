bdb4e24e245a939654f46b37bc6dc969a74de3e1
ross-brown <rbrown_98@yahoo.com>
2024-01-23 17:55:18 -0600

    fix build error in ListingList for id type

--- src/listings/ListingCard.tsx
+++ src/listings/ListingCard.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 interface ListingCardProps {
-    id: string;
+    id: string | number;
     title: string;
     price: string | number;
     photoUrl: string;
