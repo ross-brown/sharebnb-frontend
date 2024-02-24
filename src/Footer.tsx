1839b59928ca7ef04149d9a1778a80e2df6221de
ross-brown <rbrown_98@yahoo.com>
2024-02-24 14:42:00 -0600

    add hover on social icons in footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -8,13 +8,13 @@ function Footer() {
     <footer className="bg-gray-100">
       <div className="mx-auto max-w-7xl	px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
         <div className="flex justify-center md:order-2 space-x-5">
-          <a href="https://github.com/ross-brown" className="text-gray-500">
+          <a href="https://github.com/ross-brown" className="text-gray-500 hover:text-gray-700">
             <VscGithubInverted className="h-6 w-6" />
           </a>
-          <a href="https://www.linkedin.com/in/ross-brown-/" className="text-gray-500">
+          <a href="https://www.linkedin.com/in/ross-brown-/" className="text-gray-500 hover:text-gray-700">
             <FaLinkedin className="h-6 w-6" />
           </a>
-          <a href="https://rossbrowndev.com" className="text-gray-500">
+          <a href="https://rossbrowndev.com" className="text-gray-500 hover:text-gray-700">
             <CgWebsite className="h-6 w-6" />
           </a>
         </div>
