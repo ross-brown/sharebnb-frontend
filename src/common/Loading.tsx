02cf924c52ecce2da339c42403fc3bf01f50c891
ross-brown <rbrown_98@yahoo.com>
2024-02-24 12:03:08 -0600

    add spinner to loading page

--- src/common/Loading.tsx
+++ src/common/Loading.tsx
@@ -1,11 +1,10 @@
-// import "./Loading.css";
-
-/** Loading message used by components that fetch API data. */
+import { Spinner } from "./Spinner";
 
 function Loading() {
   return (
-    <div className="Loading min-h-screen flex justify-center items-center">
+    <div className="Loading min-h-screen flex flex-col justify-center items-center space-y-5">
       <h1 className="text-center font-bold text-5xl">Loading ShareBnB...</h1>
+      <Spinner />
     </div>
   );
 }
