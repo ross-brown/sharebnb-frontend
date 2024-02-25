6e171fdc145c5cd2ccc03b209d553d0839113c33
ross-brown <rbrown_98@yahoo.com>
2024-02-25 13:18:56 -0600

    update 404 page

--- src/common/NotFound.tsx
+++ src/common/NotFound.tsx
@@ -4,9 +4,20 @@ import { Link } from "react-router-dom";
 function NotFound() {
   return (
     <div className="text-center mt-64">
-      <h1 className="font-bold text-3xl">Sorry, this page isn't available.</h1>
-      <p>Either the page is broken, deleted, or doesn't exist.</p>
-      <Link to="/" className="block mt-5 text-green-500 font-bold text-xl hover:text-green-400">Go Home</Link>
+      <p className="text-base font-semibold text-green-600">404</p>
+      <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+        Page not found
+      </h1>
+      <p className="mt-6 text-base leading-7 text-gray-600">
+        Sorry, we couldn't find the page you're looking for.
+      </p>
+      <div className="mt-10 flex items-center justify-center">
+        <Link
+          to="/"
+          className="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">
+          Go Home
+        </Link>
+      </div>
     </div>
   );
 }
